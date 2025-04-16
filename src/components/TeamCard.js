cc7d2fa71c7a4f98bdf2354a0cae6e488b3694cd
Manjeet Singh <99804338+Manaregr8@users.noreply.github.com>
2025-04-16 12:12:59 +0000

    assignment3

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -1,82 +1,24 @@
 import React from "react";
 import './Hero.css';
+import ProfileCard from "./Card";
 function Team() {
     return (
         <section id="teams" className="teamSection">
-            {/*   
-     
-    <p>YOOOO</p>
-    
-    
-     */}
             <div className="teamCards">
-                <div className="teamCard">
-                    <div className="cardUpper">
-                        <img src="https://i.ibb.co/v468tGB4/f154da6a5577947ab19cf2766830ba9e.jpg" className="cardImageTeam" alt="card" />
-                        <div className="stars">
-                            <i class="fa-solid fa-star"></i>
-                        </div>
-                        <p style={{
-                            fontWeight:600,
-                        }}>Film Director, Producer</p>
-                        <div className="euro">
-                            <i class="fa-solid fa-euro-sign"></i>
-                            <p className="paraCard">19/hour</p>
-                        </div>
-                    </div>
-                    <div className="cardbelow">
-                        <p className="headerCard"> Manjeet Singh,22</p>
-                        <div className="locationCard">
-                        <i class="fa-solid fa-location-dot"></i>
-                        <p className="paraLocation">India, Delhi-110059</p>
-                        </div>
-                        <p className="Description">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-                        <div className="cardButtons">
-                            <a href="manjeet" className="cardbtn">View CV</a>
-                            <a href="singh" className="cardbtn primary">Make Offer</a>
-                        </div>
-                        <div className="statusCon">
-                            <div className="circle">
-                            </div>
-                            <p className="status">Online</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="teamCard">
-                    <div className="cardUpper">
-                        <img src="https://i.ibb.co/v468tGB4/f154da6a5577947ab19cf2766830ba9e.jpg" className="cardImageTeam" alt="card" />
-                        <div className="stars">
-                            <i class="fa-solid fa-star"></i>
-                        </div>
-                        <p style={{
-                            fontWeight:600,
-                        }}>Film Director, Producer</p>
-                        <div className="euro">
-                            <i class="fa-solid fa-euro-sign"></i>
-                            <p className="paraCard">19/hour</p>
-                        </div>
-                    </div>
-                    <div className="cardbelow">
-                        <p className="headerCard"> Manjeet Singh,22</p>
-                        <div className="locationCard">
-                        <i class="fa-solid fa-location-dot"></i>
-                        <p className="paraLocation">India, Delhi-110059</p>
-                        </div>
-                        <p className="Description">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-                        <div className="cardButtons">
-                            <a href="manjeet" className="cardbtn">View CV</a>
-                            <a href="singh" className="cardbtn primary">Make Offer</a>
-                        </div>
-                        <div className="statusCon">
-                            <div className="circle">
-                            </div>
-                            <p className="status">Online</p>
-                        </div>
-                    </div>
-                </div>
+                <ProfileCard
+                    image="https://i.ibb.co/v468tGB4/f154da6a5577947ab19cf2766830ba9e.jpg"
+                    skills={["React", "Node.js", "CSS"]}
+                    isOnline={true}
+                />
+                <ProfileCard
+                    image="https://i.ibb.co/v468tGB4/f154da6a5577947ab19cf2766830ba9e.jpg"
+                    skills={["Python", "Machine Learning", "TensorFlow"]}
+                    isOnline={false}
+                    lastSeen={new Date(new Date().setDate(new Date().getDate() - 5))} // 5 days ago
+                />
             </div>
         </section>
-
     );
 }
+
 export default Team;
